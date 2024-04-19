4323065f15a3fb38fd23e1b204c53fd919c02e06
stella-mumbi <stella.mumbi@student.moringaschool.com>
2024-04-19 22:33:54 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,16 +1,11 @@
 from flask import Flask, request, session, jsonify, make_response
-
 app = Flask(__name__)
 app.json.compact = False
-
 app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
-
 @app.route('/sessions/<string:key>', methods=['GET'])
 def show_session(key):
-
     session["hello"] = session.get("hello") or "World"
     session["goodnight"] = session.get("goodnight") or "Moon"
-
     response = make_response(jsonify({
         'session': {
             'session_key': key,
@@ -20,11 +15,8 @@ def show_session(key):
         'cookies': [{cookie: request.cookies[cookie]}
             for cookie in request.cookies],
     }), 200)
-
     response.set_cookie('mouse', 'Cookie')
-
     return response
 
 if __name__ == '__main__':
     app.run(port=5555)
-    
